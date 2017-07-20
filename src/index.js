bdb79c121708d7bbe7739a0e8c869ef5a734cce1
ajaegle <mail@ajaegle.de>
2017-07-20 20:29:53 +0200

    load image appender module on access only via System.import()

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import fooString from "./foo";
 import barString from "./bar-cjs";
 
-import {appendImageTo} from "./imageappender";
-
 console.log(fooString, barString);
 
 const button = document.querySelector("button#add");
-button.addEventListener("click", () => appendImageTo(".content"));
+const clickHandler = () => System.import("./imageappender").then(module => module.appendImageTo(".content"));
+button.addEventListener("click", clickHandler);
