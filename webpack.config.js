089e5962abb810eed204a38c0024f2e26c567e28
ajaegle <mail@ajaegle.de>
2017-07-20 19:31:09 +0200

    use bundle hashing for long time caching

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle-[hash].js",
     path: path.join(__dirname, "dist")
   },
   module: {
